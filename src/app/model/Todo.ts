663003f6536e273b5e9d17b600bfe0f4f349cefa
anum shafique <anmshafique704@gmail.com>
2024-08-04 22:49:51 +0500

    first commit

---
+++ src/app/model/Todo.ts
@@ -0,0 +1,6 @@
+export interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+    editing?: boolean; // Add an optional editing state
+  }
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from '../model/Todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -17,5 +18,28 @@ export class TodoListComponent {
     console.warn(id);
     this.list = this.list.filter(item=>item.id!==id);
 
+  }
+
+
+  todos: Todo[] = [
+    { id: 1, title: 'Todo', completed: false },
+    // { id: 2, title: 'Todo List app', completed: false }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  editTodo(todo: Todo): void {
+    todo.editing = true;
+  }
+
+  saveTodo(todo: Todo, newTitle: string): void {
+    todo.title = newTitle;
+    todo.editing = false;
+  }
+
+  cancelEdit(todo: Todo): void {
+    todo.editing = false;
   }
 }
